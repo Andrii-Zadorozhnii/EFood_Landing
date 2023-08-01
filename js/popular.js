89d06cbe73770fef7cc61336adb0c35a29a6298e
Andrii Zadorozhnii <zador746@gmail.com>
2023-07-31 22:20:51 -0700

    add generation for popular cathegory as per list and add ration and repply for a server

---
+++ js/popular.js
@@ -0,0 +1,435 @@
+const burgers = [{
+        id: 1,
+        Image: './../images/Popular/Slider/cheesburgerWithSalad.jpeg',
+
+        rating: 0,
+        name: 'Cheeseburger With Salad',
+        price: '18',
+    },
+    {
+        id: 2,
+        Image: './../images/Popular/Slider/beefBurger.png',
+        rating: 0,
+        name: 'Beef Burger',
+        price: '15',
+    },
+    {
+        id: 3,
+        Image: './../images/Popular/Slider/RoyalChesburger.png',
+        rating: 0,
+        name: 'Royal Cheesburger',
+        price: '16',
+    },
+    {
+        id: 4,
+        Image: './../images/Popular/Slider/blackBurger.png',
+        rating: 0,
+        name: 'Black Gamburger',
+        price: '14',
+    },
+    {
+        id: 5,
+        Image: './../images/Popular/Slider/chickenBurger.png',
+        rating: 0,
+        name: 'Chicken Burger',
+        price: '15',
+    },
+];
+
+const pizza = [{
+    id: 1,
+    Image: './../images/Popular/Slider/cheesburgerWithSalad.jpeg',
+    rating: 0,
+    name: 'Margherita',
+    price: '12',
+    category: 'Classic Pizza',
+  },
+  {
+    id: 2,
+    Image: './../images/Popular/Slider/beefBurger.png',
+    rating: 0,
+    name: 'Pepperoni',
+    price: '15',
+    category: 'Classic Pizza',
+  },
+  {
+    id: 3,
+    Image: './../images/Popular/Slider/RoyalChesburger.png',
+    rating: 0,
+    name: 'Hawaiian',
+    price: '14',
+    category: 'Classic Pizza',
+  },
+  {
+    id: 4,
+    Image: './../images/Popular/Slider/blackBurger.png',
+    rating: 0,
+    name: 'Vegetarian',
+    price: '13',
+    category: 'Classic Pizza',
+  },
+  {
+    id: 5,
+    Image: './../images/Popular/Slider/chickenBurger.png',
+    rating: 0,
+    name: 'BBQ Chicken',
+    price: '16',
+    category: 'Classic Pizza',
+  },
+  {
+    id: 6,
+    Image: './../images/Popular/Slider/pizza1.jpeg',
+    rating: 0,
+    name: 'Four Cheese',
+    price: '17',
+    category: 'Specialty Pizza',
+  },
+  {
+    id: 7,
+    Image: './../images/Popular/Slider/pizza2.jpeg',
+    rating: 0,
+    name: 'Meat Lovers',
+    price: '18',
+    category: 'Specialty Pizza',
+  },
+  {
+    id: 8,
+    Image: './../images/Popular/Slider/pizza3.jpeg',
+    rating: 0,
+    name: 'Mediterranean',
+    price: '16',
+    category: 'Specialty Pizza',
+  },
+  {
+    id: 9,
+    Image: './../images/Popular/Slider/pizza4.jpeg',
+    rating: 0,
+    name: 'Pesto Chicken',
+    price: '17',
+    category: 'Specialty Pizza',
+  },
+  {
+    id: 10,
+    Image: './../images/Popular/Slider/pizza5.jpeg',
+    rating: 0,
+    name: 'Supreme',
+    price: '19',
+    category: 'Specialty Pizza',
+  },
+]
+
+const sandwich = [{
+    id: 1,
+    Image: './../images/Popular/Slider/sandwich1.jpeg',
+    rating: 0,
+    name: 'Classic Club',
+    price: '9',
+    category: 'Classic Sandwich',
+  },
+  {
+    id: 2,
+    Image: './../images/Popular/Slider/sandwich2.jpeg',
+    rating: 0,
+    name: 'BLT',
+    price: '8',
+    category: 'Classic Sandwich',
+  },
+  {
+    id: 3,
+    Image: './../images/Popular/Slider/sandwich3.jpeg',
+    rating: 0,
+    name: 'Turkey Avocado',
+    price: '10',
+    category: 'Classic Sandwich',
+  },
+  {
+    id: 4,
+    Image: './../images/Popular/Slider/sandwich4.jpeg',
+    rating: 0,
+    name: 'Veggie Delight',
+    price: '7',
+    category: 'Classic Sandwich',
+  },
+  {
+    id: 5,
+    Image: './../images/Popular/Slider/sandwich5.jpeg',
+    rating: 0,
+    name: 'Italian Sub',
+    price: '9',
+    category: 'Classic Sandwich',
+  },
+  {
+    id: 6,
+    Image: './../images/Popular/Slider/sandwich6.jpeg',
+    rating: 0,
+    name: 'Chicken Caesar Wrap',
+    price: '8',
+    category: 'Specialty Sandwich',
+  },
+  {
+    id: 7,
+    Image: './../images/Popular/Slider/sandwich7.jpeg',
+    rating: 0,
+    name: 'BBQ Pulled Pork',
+    price: '9',
+    category: 'Specialty Sandwich',
+  },
+  {
+    id: 8,
+    Image: './../images/Popular/Slider/sandwich8.jpeg',
+    rating: 0,
+    name: 'Caprese Panini',
+    price: '10',
+    category: 'Specialty Sandwich',
+  },
+  {
+    id: 9,
+    Image: './../images/Popular/Slider/sandwich9.jpeg',
+    rating: 0,
+    name: 'Philly Cheesesteak',
+    price: '11',
+    category: 'Specialty Sandwich',
+  },
+  {
+    id: 10,
+    Image: './../images/Popular/Slider/sandwich10.jpeg',
+    rating: 0,
+    name: 'Tuna Melt',
+    price: '8',
+    category: 'Specialty Sandwich',
+  },
+]
+
+const asianFood = [{
+    id: 1,
+    Image: 'https://example.com/images/asian1.jpeg',
+    rating: 0,
+    name: 'Chicken Teriyaki',
+    price: '12',
+    category: 'Japanese Cuisine',
+  },
+  {
+    id: 2,
+    Image: 'https://example.com/images/asian2.jpeg',
+    rating: 0,
+    name: 'Beef Stir Fry',
+    price: '14',
+    category: 'Chinese Cuisine',
+  },
+  {
+    id: 3,
+    Image: 'https://example.com/images/asian3.jpeg',
+    rating: 0,
+    name: 'Pad Thai',
+    price: '11',
+    category: 'Thai Cuisine',
+  },
+  {
+    id: 4,
+    Image: 'https://example.com/images/asian4.jpeg',
+    rating: 0,
+    name: 'Sushi Platter',
+    price: '18',
+    category: 'Japanese Cuisine',
+  },
+  {
+    id: 5,
+    Image: 'https://example.com/images/asian5.jpeg',
+    rating: 0,
+    name: 'Kung Pao Chicken',
+    price: '13',
+    category: 'Chinese Cuisine',
+  },
+  {
+    id: 6,
+    Image: 'https://example.com/images/asian6.jpeg',
+    rating: 0,
+    name: 'Green Curry',
+    price: '15',
+    category: 'Thai Cuisine',
+  },
+  {
+    id: 7,
+    Image: 'https://example.com/images/asian7.jpeg',
+    rating: 0,
+    name: 'Yakitori Skewers',
+    price: '10',
+    category: 'Japanese Cuisine',
+  },
+  {
+    id: 8,
+    Image: 'https://example.com/images/asian8.jpeg',
+    rating: 0,
+    name: 'Mapo Tofu',
+    price: '12',
+    category: 'Chinese Cuisine',
+  },
+  {
+    id: 9,
+    Image: 'https://example.com/images/asian9.jpeg',
+    rating: 0,
+    name: 'Tom Yum Soup',
+    price: '9',
+    category: 'Thai Cuisine',
+  },
+  {
+    id: 10,
+    Image: 'https://example.com/images/asian10.jpeg',
+    rating: 0,
+    name: 'Sashimi Platter',
+    price: '20',
+    category: 'Japanese Cuisine',
+  },
+]
+
+const setMenu = [{
+        id: 1,
+        Image: './../images/Popular/Slider/cheesburgerWithSalad.jpeg',
+
+        rating: 0,
+        name: 'Cheeseburger With Salad',
+        price: '18',
+    },
+    {
+        id: 2,
+        Image: './../images/Popular/Slider/beefBurger.png',
+        rating: 0,
+        name: 'Beef Burger',
+        price: '15',
+    },
+    {
+        id: 3,
+        Image: './../images/Popular/Slider/RoyalChesburger.png',
+        rating: 0,
+        name: 'Royal Cheesburger',
+        price: '16',
+    },
+    {
+        id: 4,
+        Image: './../images/Popular/Slider/blackBurger.png',
+        rating: 0,
+        name: 'Black Gamburger',
+        price: '14',
+    },
+    {
+        id: 5,
+        Image: './../images/Popular/Slider/chickenBurger.png',
+        rating: 0,
+        name: 'Chicken Burger',
+        price: '15',
+    },
+]
+
+
+function createProductElement(product) {
+    const productElement = document.createElement('div');
+    productElement.classList.add('product');
+    productElement.innerHTML = `
+      <div class="slider-card">
+        <img class="slider-card__image" src="${product.Image}" alt="${product.name}">
+        <div class="rating slider-card__rating" 
+             onmousemove="highlightStars(event, ${product.id})"
+             onmouseleave="resetStars(${product.id})">
+          <span class="star" onclick="rate(${product.id}, 1)">&#9733;</span>
+          <span class="star" onclick="rate(${product.id}, 2)">&#9733;</span>
+          <span class="star" onclick="rate(${product.id}, 3)">&#9733;</span>
+          <span class="star" onclick="rate(${product.id}, 4)">&#9733;</span>
+          <span class="star" onclick="rate(${product.id}, 5)">&#9733;</span>
+        </div>
+        <h3 class="slider-card__name">${product.name}</h3>
+        <p class="slider-card__price">$${product.price}</p>
+        <button class="btn slider-card__button">Add to Cart</button>
+      </div>
+    `;
+    return productElement;
+}
+
+function highlightStars(event, productId) {
+    const stars = document.querySelectorAll(`.product[data-id="${productId}"] .star`);
+    const mouseX = event.clientX;
+
+    stars.forEach((star, index) => {
+        const starX = star.getBoundingClientRect().left;
+        if (mouseX >= starX) {
+            star.classList.add('active');
+        } else {
+            star.classList.remove('active');
+        }
+    });
+}
+
+function resetStars(productId) {
+    const stars = document.querySelectorAll(`.product[data-id="${productId}"] .star`);
+    stars.forEach((star) => {
+        star.classList.remove('active');
+    });
+}
+
+async function rate(productId, stars) {
+    const productElement = document.querySelector(`.product[data-id="${productId}"]`);
+    const product = burgers.find((p) => p.id === productId);
+
+    product.rating = stars;
+    const starsElements = productElement.querySelectorAll('.star');
+    starsElements.forEach((star, index) => {
+        if (index < stars) {
+            star.classList.add('active');
+        } else {
+            star.classList.remove('active');
+        }
+    });
+
+    try {
+        const response = await fetch('URL_ВАШЕГО_СЕРВЕРА', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                productId,
+                stars
+            }),
+        });
+
+        if (response.ok) {
+            console.log('Рейтинг успешно отправлен на сервер.');
+        } else {
+            console.log('Ошибка при отправке рейтинга на сервер:', response.status);
+        }
+    } catch (error) {
+        console.log('Произошла ошибка при отправке рейтинга:', error.message);
+    }
+}
+
+function updateProducts(category) {
+    const products = category;
+    const productsContainer = document.getElementById('products-container');
+    productsContainer.innerHTML = '';
+
+    products.forEach((product) => {
+        const productElement = createProductElement(product);
+        productElement.setAttribute('data-id', product.id);
+        productsContainer.appendChild(productElement);
+    });
+}
+
+const productsContainer = document.getElementById('products-container');
+updateProducts(burgers);
+
+const categoryButtons = document.querySelectorAll('.category-button');
+categoryButtons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        const selectedCategory = event.target.dataset.category;
+        if (selectedCategory === 'burger') {
+            updateProducts(burgers);
+        } else if (selectedCategory === 'pizza') {
+            updateProducts(pizza);
+        } else if (selectedCategory === 'sandwich') {
+            updateProducts(sandwich);
+        } else if (selectedCategory === 'asianFood') {
+            updateProducts(asianFood);
+        } else if (selectedCategory === 'setMenu') {
+            updateProducts(setMenu);
+        }
+    });
+});
---
+++ js/slider.js
@@ -0,0 +1,13 @@
+function highlightStars(event) {
+    const stars = document.querySelectorAll('.star');
+    const mouseX = event.clientX;
+
+    stars.forEach((star, index) => {
+        const starX = star.getBoundingClientRect().left;
+        if (mouseX >= starX) {
+            star.classList.add('active');
+        } else {
+            star.classList.remove('active');
+        }
+    });
+}
